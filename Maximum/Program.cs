721a21491d23ad2124689853a0e86a0f6fe0d34d
Mészáros Árpád <meszaros.arpad@outlook.hu>
2022-01-31 18:40:30 +0100

    Add project files.

---
+++ Maximum/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maximum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            List<int> list = new List<int>();
+
+            int maximum = 0;
+
+            for (int i = 0; i < 20; i++)
+            {
+                int szam = rnd.Next(1, 101);
+                list.Add(szam);
+                Console.WriteLine(szam);
+                if (maximum < szam) maximum = szam;
+            }
+            Console.WriteLine(" ");
+            Console.WriteLine($"A legnagyobb elem a listában: {maximum}");
+            Console.ReadKey();
+        }
+    }
+}
